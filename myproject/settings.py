ba93dd3b6430cfc7ad65f4a3d1a8af1b6c9104b9
Luiz Paulo Coutinho <coutinholps@gmail.com>
2024-12-13 10:20:08 -0300

    update settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
